6e6f9ab556c4f6cc40e3f8eb6a210d11f2691966
James Fuller <kux9@cdc.gov>
2021-08-30 15:11:29 -0400

    testing still
    figuring out roxygen2

--- R/get_godata_version.R
+++ R/get_godata_version.R
@@ -1,11 +1,10 @@
 get_godata_version <- function(url=url) {
 
-  version.request <- httr::GET(paste0(url,"api/system-settings/version"))
+  version.request <- GET(paste0(url,"api/system-settings/version"))
 
   if (version.request$status_code==200) {
-    versionJSON <- httr::content(version.request, as="text")
-    version <- jsonlite::fromJSON(versionJSON, flatten=TRUE)
-    message(paste0("Version ", version$version))
+    versionJSON <- content(version.request, as="text")
+    version <- fromJSON(versionJSON, flatten=TRUE)
     return(version$version)
   } else {
     stop(paste0("Error ",version.request$status_code))
