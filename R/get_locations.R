2041e35560e53104d2a04d22a3e3fbe34879c6fc
Sara Hollis <57128361+sarahollis@users.noreply.github.com>
2021-09-13 13:39:25 +0200

    Create get_locations.R

---
+++ R/get_locations.R
@@ -0,0 +1,35 @@
+#' Get a list of all locations and their attributes, across all outbreaks on the instance (since this is a system-level API endpoint).
+#'
+#' @param url Insert the base URL for your instance of Go.Data here. Don't forget the forward slash "/" at end!
+#' @param username The email address for your Go.Data login.
+#' @param password The password for your Go.Data login
+#'
+#' @return
+#' Returns data frame of locations associated with Go.Data instance.
+#' @export
+#' @examples
+#' url <- "https://MyGoDataServer.com/"
+#' username <- "myemail@email.com"
+#' password <- "mypassword"
+#'
+#' users <- get_users(url=url, username=username, password=password)
+#' @importFrom magrittr %>%
+#' @import dplyr
+#' @import tidyr
+#' @import httr
+#' @importFrom jsonlite fromJSON
+#' @importFrom purrr pluck
+#' @export
+
+
+get_locations <- function(url=url, username=username, password=password) {
+  
+  users <- GET(paste0(url,"api/locations",
+                      "?access_token=",godataR::get_access_token(url=url, username=username, password=password))) %>%
+    content(as="text") %>%
+    fromJSON(flatten=TRUE) %>%
+    filter(deleted!=TRUE) 
+  
+  return(locations)
+  
+}
