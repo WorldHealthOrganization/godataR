725bcdd93cfddaf8225b1dfadaacde462d36cb0a
Sara Hollis <57128361+sarahollis@users.noreply.github.com>
2021-09-14 13:30:12 +0200

    Update get_events.R

---
+++ R/get_events.R
@@ -0,0 +1,79 @@
+' Download events from Go.Data (version 2.38.0 or earlier)
+#'
+#' @param url Insert the base URL for your instance of Go.Data here. Don't forget the forward slash "/" at end!
+#' @param username The email address for your Go.Data login.
+#' @param password The password for your Go.Data login
+#' @param outbreak_id The id number for the outbreak for which you want to download events.
+#' @param batch_size For large datasets, specify the number of records to retrieve in each iteration.
+#'
+#' @return
+#' Returns data frame of events. may require further unnesting. See the tidyr::unnest() function.
+#' @export
+#'
+#' @examples
+#' url <- "https://MyGoDataServer.com/"
+#' username <- "myemail@email.com"
+#' password <- "mypassword"
+#' outbreak_id <- "3b5554d7-2c19-41d0-b9af-475ad25a382b"
+#'
+#' events <- get_events(url=url, username=username, password=password, outbreak_id=outbreak_id)
+#' @importFrom magrittr %>%
+#' @import dplyr
+#' @import tidyr
+#' @import httr
+#' @import tibble
+#' @importFrom jsonlite fromJSON
+#' @importFrom purrr pluck
+get_events <- function(url=url, username=username, password=password, outbreak_id=outbreak_id, batch_size=50000) {
+
+  #Check version of Go.Data
+  if (check_godata_version(url=url)==TRUE) {
+    warning("Your version of Go.Data is 2.38.1 or later. Consider using the function get_events2().")
+  }
+
+  #Check that outbreak_id is active
+  if (outbreak_id != get_active_outbreak(url=url, username=username, password=password)) {
+    set_active_outbreak(url=url, username=username, password=password, outbreak_id=outbreak_id)
+  }
+
+  #get total number of records
+  df_n <- GET(paste0(url,"api/outbreaks/",outbreak_id,"/events/filtered-count"),
+                    add_headers(Authorization = paste("Bearer", get_access_token(url=url, username=username, password=password), sep = " "))) %>%
+    content(as="text") %>%
+    fromJSON(flatten=TRUE) %>%
+    unlist() %>%
+    unname()
+
+  #Import records in batches
+  df <- tibble()
+  batch_size <- batch_size # number of records to import per iteration
+  skip <-0
+  message("****************************")
+
+  #Download records in batches, and then append them into a single dataset
+  while (skip < df_n) {
+
+    #Progress message
+    if (df_n <= batch_size) message(paste0("...downloading records 1 to ",df_n))
+    if (df_n > batch_size)     message(paste0("...downloading records ", as.character(skip+1, scientific = FALSE), " to ", format(skip+batch_size, scientific = FALSE)))
+
+    #fetch the batch of records
+    df.i <- GET(paste0(url,"api/outbreaks/",outbreak_id,"/events",
+                             "/?filter={%22limit%22:",format(batch_size, scientific = FALSE),",%22skip%22:",format(skip, scientific = FALSE),"}"),
+                      add_headers(Authorization = paste("Bearer", get_access_token(url=url, username=username, password=password), sep = " "))) %>%
+      content(as='text') %>%
+      fromJSON( flatten=TRUE) %>%
+      as_tibble()
+
+    #append the new batch of records to the existing data frame
+    df <- df %>%
+      bind_rows(df.i)
+
+    #update numbers for the next iteration
+    skip <- skip + batch_size
+    rm(df.i)
+  }
+  rm(batch_size, skip, df_n)
+
+  return(df)
+}
