f72716993a68627824a13c90011486bc33ca4f04
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-01-19 16:54:44 +0000

    linted style of get_cases

--- R/get_cases.R
+++ R/get_cases.R
@@ -22,17 +22,27 @@
 #'    be the default if you are using Go.Data version 2.38.1
 #'    or newer.
 #'
-#' @param url Insert the base URL for your instance of Go.Data here. Don't forget the forward slash "/" at end!
+#' @param url Insert the base URL for your instance of Go.Data here.
+#' Don't forget the forward slash "/" at end!
 #' @param username The email address for your Go.Data login.
 #' @param password The password for your Go.Data login
-#' @param outbreak_id The id number for the outbreak for which you want to download data.
-#' @param method The method to download data. `method="export"` is the preferred and default method for Go.Data version 2.38.1 or later. See Details.
-#' @param batch_size If `method="batches"`, then `batch_size` specifies the number of records to retrieve in each iteration.
-#' @param wait If `method="export"`, then `wait` is the number of seconds to wait in between iterations of checking the status of the export.
-#' @param file.type If `method="export"`, then `file.type` determines Whether the resulting data frame should contain nested fields (`file.type="json"`, the default) or an entirely flat data structure (`file.type="csv"`)
+#' @param outbreak_id The id number for the outbreak for which you want to
+#' download data.
+#' @param method The method to download data. `method="export"` is the
+#' preferred and default method for Go.Data version 2.38.1 or later.
+#' See Details.
+#' @param batch_size If `method = "batches"`, then `batch_size` specifies the
+#' number of records to retrieve in each iteration.
+#' @param wait If `method = "export"`, then `wait` is the number of seconds to
+#' wait in between iterations of checking the status of the export.
+#' @param file.type If `method = "export"`, then `file.type` determines Whether
+#' the resulting data frame should contain nested fields (`file.type = "json"`,
+#' the default) or an entirely flat data structure (`file.type = "csv"`)
 #'
 #' @return
-#' Returns a data frame. Some fields, such as addresses, hospitalization history, and questionnaire fields may require further unnesting. See \code{\link[tidyr]{nest}} for assitance with unnesting.
+#' Returns a data frame. Some fields, such as addresses, hospitalization
+#' history, and questionnaire fields may require further unnesting. See
+#' `\link[tidyr]{nest}` for assitance with unnesting.
 #' @export
 #'
 #' @examples
@@ -56,22 +66,32 @@
 #' @importFrom purrr pluck
 
 
-get_cases <- function(url=url,
-                      username=username,
-                      password=password,
-                      outbreak_id=outbreak_id,
-                      method=c("export","batch"),
-                      batch_size=50000,
-                      wait=2,
-                      file.type=c("json","csv")) {
+get_cases <- function(url = url,
+                      username = username,
+                      password = password,
+                      outbreak_id = outbreak_id,
+                      method = c("export", "batch"),
+                      batch_size = 50000,
+                      wait = 2,
+                      file.type = c("json", "csv")) {
 
   #Check that outbreak_id is active
-  if (outbreak_id != get_active_outbreak(url=url, username=username, password=password)) {
-    set_active_outbreak(url=url, username=username, password=password, outbreak_id=outbreak_id)
+  active_outbreak_id <- get_active_outbreak(
+    url = url,
+    username = username,
+    password = password
+  )
+  if (outbreak_id != active_outbreak_id) {
+    set_active_outbreak(
+      url = url,
+      username = username,
+      password = password,
+      outbreak_id = outbreak_id
+    )
   }
 
   #Set default method based on current version of Go.Data
-  version.check <- check_godata_version(url=url)
+  version.check <- check_godata_version(url = url)
   if (!version.check) {
     method <- "batches" #Older version of Go.Data can only use the batch method
   } else if (missing(method)) {
@@ -81,14 +101,16 @@ get_cases <- function(url=url,
 
   if (method == "batches") {
 
-    api_call_n <- paste0(url, "api/outbreaks/",outbreak_id,"/cases/count")
-    api_call_get <- paste0(url, "api/outbreaks/",outbreak_id,"/cases")
-    df <- batch_downloader(url=url,
-                           username=username,
-                           password=password,
-                           api_call_n=api_call_n,
-                           api_call_get=api_call_get,
-                           batch_size=batch_size)
+    api_call_n <- paste0(url, "api/outbreaks/", outbreak_id, "/cases/count")
+    api_call_get <- paste0(url, "api/outbreaks/", outbreak_id, "/cases")
+    df <- batch_downloader(
+      url = url,
+      username = username,
+      password = password,
+      api_call_n = api_call_n,
+      api_call_get = api_call_get,
+      batch_size = batch_size
+    )
 
   } else if (method == "export") {
 
@@ -96,13 +118,15 @@ get_cases <- function(url=url,
     if (missing(file.type)) file.type <- "json"
 
     #Submit an export request to the system
-    api_call_request <- paste0(url,"api/outbreaks/",outbreak_id,"/cases/export")
-    df <- export_downloader(url=url,
-                            username=username,
-                            password=password,
-                            api_call_request=api_call_request,
-                            file.type = file.type,
-                            wait = wait)
+    api_call_request <- paste0(url, "api/outbreaks/", outbreak_id, "/cases/export")
+    df <- export_downloader(
+      url = url,
+      username = username,
+      password = password,
+      api_call_request = api_call_request,
+      file.type = file.type,
+      wait = wait
+    )
 
 
   }
