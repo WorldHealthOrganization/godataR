7ae103bde3362c3a9ac2a1e197bcf3930e88ed4c
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-01-20 13:45:25 +0000

    linted export_downloader

--- R/export_downloader.R
+++ R/export_downloader.R
@@ -2,15 +2,21 @@
 #'
 #' A housekeeping function to do export requests & downloads.
 #'
-#' @param url Insert the base URL for your instance of Go.Data here. Don't forget the forward slash "/" at end!
+#' @param url Insert the base URL for your instance of Go.Data here. Don't
+#' forget the forward slash "/" at end!
 #' @param username The email address for your Go.Data login.
 #' @param password The password for your Go.Data login
 #' @param api_call_request The API url to get the number of records.
-#' @param wait The number of seconds to wait in between iterations of checking the status of the export.
-#' @param file.type Whether the resulting data frame should contain nested fields (`file.type="json"`, the default) or an entirely flat data structure (`file.type="csv"`)
-
+#' @param wait The number of seconds to wait in between iterations of checking
+#' the status of the export.
+#' @param file.type Whether the resulting data frame should contain nested
+#' fields (`file.type = "json"`, the default) or an entirely flat data structure
+#' (`file.type = "csv"`)
+#'
 #' @return
-#' Returns a data frame. Some fields, such as addresses, hospitalization history, and questionnaire fields may require further unnesting. See \code{\link[tidyr]{nest}} for assitance with unnesting.
+#' Returns a data frame. Some fields, such as addresses, hospitalization
+#' history, and questionnaire fields may require further unnesting. See
+#' `\link[tidyr]{nest}` for assitance with unnesting.
 #'
 #' @examples
 #' \dontrun{
@@ -19,10 +25,12 @@
 #' password <- "mypassword"
 #' outbreak_id <- "3b5554d7-2c19-41d0-b9af-475ad25a382b"
 #'
-#' cases <- get_cases(url=url,
-#'                    username=username,
-#'                    password=password,
-#'                    outbreak_id=outbreak_id)
+#' cases <- get_cases(
+#'   url = url,
+#'   username = username,
+#'   password = password,
+#'   outbreak_id = outbreak_id
+#' )
 #' }
 #' @importFrom magrittr %>%
 #' @import dplyr
@@ -32,8 +40,6 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom purrr pluck
 #' @importFrom utils read.csv
-#'
-#'
 export_downloader <- function(url = url,
                              username = username,
                              password = password,
@@ -41,37 +47,100 @@ export_downloader <- function(url = url,
                              wait = wait,
                              file.type = file.type) {
 
-  request_id <- GET(paste0(api_call_request,
-                           "?filter=%7B%22where%22%3A%7B%22useDbColumns%22%3A%22true%22%2C%20%22dontTranslateValues%22%3A%22true%22%2C%20%22jsonReplaceUndefinedWithNull%22%3A%22true%22%20%7D%7D",
-                           "&type=",file.type,
-                           "&access_token=",get_access_token(url=url, username=username, password=password))) %>%
+  request_id <- GET(
+    paste0(
+      api_call_request,
+      "?filter=%7B%22where%22%3A%7B%22useDbColumns%22%3A%22true%22%2C%20%22",
+      "dontTranslateValues%22%3A%22true%22%2C%20%22",
+      "jsonReplaceUndefinedWithNull%22%3A%22true%22%20%7D%7D",
+      "&type=",
+      file.type,
+      "&access_token=",
+      get_access_token(
+        url = url,
+        username = username,
+        password = password
+      )
+    )
+  ) %>%
     content() %>%
     pluck("exportLogId")
 
 
   #Check status of request periodcially, until finished
-  #function argument 'wait' determines the number of seconds to wait between iterations
-  export.request.status <- get_export_status(url=url, username=username, password=password, request_id=request_id)
+  #function argument 'wait' determines the number of seconds to wait between
+  #iterations
+  export_request_status <- get_export_status(
+    url = url,
+    username = username,
+    password = password,
+    request_id = request_id
+  )
 
-  while(export.request.status$statusStep != "LNG_STATUS_STEP_EXPORT_FINISHED") {
+  status_step <- export_request_status$statusStep
+  while (status_step != "LNG_STATUS_STEP_EXPORT_FINISHED") {
     Sys.sleep(wait)
-    export.request.status <- GET(paste0(url,"api/export-logs/",request_id,"?access_token=",get_access_token(url=url, username=username, password=password))) %>%
+    export_request_status <- GET(
+      paste0(
+        url,
+        "api/export-logs/",
+        request_id,
+        "?access_token=",
+        get_access_token(
+          url = url,
+          username = username,
+          password = password
+        )
+      )
+    ) %>%
       content()
-    message(paste0("...processed ",export.request.status$processedNo, " of ", export.request.status$totalNo, " records"))
+    message(
+      paste0(
+        "...processed ",
+        export_request_status$processedNo,
+        " of ",
+        export_request_status$totalNo,
+        " records"
+      )
+    )
   }
 
   #Download the export
   message("...beginning download")
-  if (file.type=="json") {
-    df <- GET(paste0(url,"api/export-logs/",request_id,"/download?access_token=",get_access_token(url=url, username=username, password=password))) %>%
-      content("text", encoding="UTF-8") %>%
-      fromJSON(flatten=TRUE)
+  if (file.type == "json") {
+    df <- GET(
+      paste0(
+        url,
+        "api/export-logs/",
+        request_id,
+        "/download?access_token=",
+        get_access_token(
+          url = url,
+          username = username,
+          password = password
+        )
+      )
+    ) %>%
+      content("text", encoding = "UTF-8") %>%
+      fromJSON(flatten = TRUE)
 
     # fix one strange variable name
     names(df)[names(df) %in% "_id"] <- "id"
-  } else if (file.type=="csv") {
-    df <- GET(paste0(url,"api/export-logs/",request_id,"/download?access_token=",get_access_token(url=url, username=username, password=password))) %>%
-      content("text", encoding="UTF-8") %>%
+  } else if (file.type == "csv") {
+    df <- GET(
+      paste0(
+        url,
+        "api/export-logs/",
+        request_id,
+        "/download?access_token=",
+        get_access_token(
+          url = url,
+          username = username,
+          password = password
+        )
+      )
+    ) %>%
+      content("text", encoding = "UTF-8") %>%
       textConnection() %>%
       read.csv()
     names(df)[names(df) %in% "X_id"] <- "id"
@@ -80,5 +149,3 @@ export_downloader <- function(url = url,
   message("...download complete!")
   return(df)
 }
-
-
