1b5ccc966539b91886b6a61a2a615dcc086deeee
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-02-01 16:31:45 +0000

    linted get_all_outbreaks

--- R/get_all_outbreaks.R
+++ R/get_all_outbreaks.R
@@ -5,12 +5,15 @@
 #' housekeeping function used in many of the
 #' other `godataR` functions.
 #'
-#' @param url Insert the base URL for your instance of Go.Data here. Don't forget the forward slash "/" at end!
+#' @param url Insert the base URL for your instance of Go.Data here. Don't
+#' forget the forward slash "/" at end!
 #' @param username The email address for your Go.Data login.
 #' @param password The password for your Go.Data login
 #'
 #' @return
-#' Returns data frame of outbreaks. The resulting list is filtered by the user's permissions: only outbreaks for which the user has access will be returned.
+#' Returns data frame of outbreaks. The resulting list is filtered by the
+#' user's permissions: only outbreaks for which the user has access will be
+#' returned.
 #' @export
 #' @examples
 #' \dontrun{
@@ -29,18 +32,26 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom purrr pluck
 #' @export
+get_all_outbreaks <- function(url = url,
+                              username = username,
+                              password = password) {
 
-get_all_outbreaks <- function(url=url,
-                              username=username,
-                              password=password) {
-
-  outbreaks <- GET(paste0(url,"api/outbreaks",
-                                 "?access_token=",get_access_token(url=url, username=username, password=password))) %>%
-    content(as="text") %>%
-    fromJSON(flatten=TRUE) %>%
-    filter(deleted!=TRUE) %>%
-    select(any_of(c("id", "name", "description","createdBy","createdAt")))
+  outbreaks <- GET(
+    paste0(
+      url,
+      "api/outbreaks",
+      "?access_token=",
+      get_access_token(
+        url = url,
+        username = username,
+        password = password
+      )
+    )
+  ) %>%
+    content(as = "text") %>%
+    fromJSON(flatten = TRUE) %>%
+    filter(.data$deleted != TRUE) %>%
+    select(any_of(c("id", "name", "description", "createdBy", "createdAt")))
 
   return(outbreaks)
-
 }
