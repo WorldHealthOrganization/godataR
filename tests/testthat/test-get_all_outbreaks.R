475cd2f17c626d36efabc833926ee63cab5ec5f3
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-02-01 17:01:43 +0000

    added test for get_all_outbreaks (skipped by default)

---
+++ tests/testthat/test-get_all_outbreaks.R
@@ -0,0 +1,21 @@
+test_that("get_all_outbreaks works as expected", {
+  skip("get_all_outbreaks requires API call")
+
+  res <- get_all_outbreaks(
+    url = url,
+    username = username,
+    password = password
+  )
+
+  expect_s3_class(res, "tbl_df")
+  expect_s3_class(res, "data.frame")
+  expect_identical(dim(res), c(10L, 5L))
+  expect_identical(
+    colnames(res),
+    c("id", "name", "description", "createdBy", "createdAt" )
+  )
+  expect_identical(
+    unname(sapply(res[1, ], class)),
+    rep("character", 5)
+  )
+})
