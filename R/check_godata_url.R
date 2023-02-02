cf14b9096cd586dfc71efbd18190f9af97c1b5d3
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-02-02 10:35:55 +0000

    removed pipe from check_godata_url

--- R/check_godata_url.R
+++ R/check_godata_url.R
@@ -21,8 +21,9 @@
 check_godata_url <- function(url) {
 
   # Get status code for version check
-  status_code <- GET(paste0(url, "api/system-settings/version")) %>%
-    pluck("status_code")
+  status_code <- GET(paste0(url, "api/system-settings/version"))
+
+  status_code <- pluck(status_code, "status_code")
 
   # create boolean based on status code being 200 (success)
   check <- (status_code == 200)
