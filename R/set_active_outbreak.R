a8fe768d61e551c981a11532587e6f490b129216
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-02-02 11:56:16 +0000

    linted set_active_outbreak

--- R/set_active_outbreak.R
+++ R/set_active_outbreak.R
@@ -8,7 +8,8 @@
 #' Each Go.Data user can have 1 and only 1 active
 #' outbreak at a given time.
 #'
-#' @param url Insert the base URL for your instance of Go.Data here. Don't forget the forward slash "/" at end!
+#' @param url Insert the base URL for your instance of Go.Data here. Don't
+#' forget the forward slash "/" at end!
 #' @param username The email address for your Go.Data login.
 #' @param password The password for your Go.Data login
 #' @param outbreak_id The id number for the outbreak to set to active.
@@ -34,37 +35,77 @@
 #' @import httr
 #' @importFrom jsonlite fromJSON
 
-set_active_outbreak <- function(url=url,
-                                username=username,
-                                password=password,
-                                outbreak_id=outbreak_id) {
+set_active_outbreak <- function(url = url,
+                                username = username,
+                                password = password,
+                                outbreak_id = outbreak_id) {
 
 
   #Get User ID & Active Outbreak ID
-  user.details <- GET(paste0(url,"api/users",
-                      "?access_token=",get_access_token(url=url, username=username, password=password))) %>%
-    content(as="text") %>%
-    fromJSON(flatten=TRUE) %>%
-    filter(email==username)
+  user_details <- GET(
+    paste0(
+      url,
+      "api/users",
+      "?access_token=",
+      get_access_token(
+        url = url,
+        username = username,
+        password = password
+      )
+    )
+  ) %>%
+    content(as = "text") %>%
+    fromJSON(flatten = TRUE) %>%
+    filter(.data$email == username)
 
-  current.active.outbreak <- user.details$activeOutbreakId
-  user.id <- user.details$id
+  current_active_outbreak <- user_details$activeOutbreakId
+  user_id <- user_details$id
 
   #Get List of Available Outbreak IDs
-  available.outbreaks <- get_all_outbreaks(url=url, username=username, password=password) %>%
-    select(id) %>% unlist()
+  available_outbreaks <- get_all_outbreaks(
+    url = url,
+    username = username,
+    password = password
+  ) %>%
+    select(id) %>%
+    unlist()
 
-  if (current.active.outbreak == outbreak_id) {   #Is outbreak_id already active?
-    text <- paste0("Active outbreak not changed. ", outbreak_id, " is already active.")
-  } else if (!(outbreak_id %in% available.outbreaks)) {
-    stop(paste0("Active outbreak not changed. ",outbreak_id, " not in list of user's available outbreaks. Make sure the id number is correct & that the user has proper access."))
+  if (current_active_outbreak == outbreak_id) { # Is outbreak_id already active?
+    text <- paste0(
+      "Active outbreak not changed. ",
+      outbreak_id,
+      " is already active."
+    )
+  } else if (!(outbreak_id %in% available_outbreaks)) {
+    stop(paste0(
+      "Active outbreak not changed. ",
+      outbreak_id,
+      " not in list of user's available outbreaks. Make sure the id number is",
+      " correct & that the user has proper access."
+    ))
   } else {
 
-    new.data <- list("activeOutbreakId"=outbreak_id)
-    patch.active.outbreak <- PATCH(paste0(url,"api/users/",user.id),
-                                   add_headers(Authorization = paste("Bearer", get_access_token(url=url, username=username, password=password), sep = " ")),
-                                   body=new.data,
-                                   encode="json")
+    new_data <- list("activeOutbreakId" = outbreak_id)
+    patch_active_outbreak <- PATCH(
+      paste0(
+        url,
+        "api/users/",
+        user_id
+      ),
+      add_headers(
+        Authorization = paste(
+          "Bearer",
+          get_access_token(
+            url = url,
+            username = username,
+            password = password
+          ),
+          sep = " "
+        )
+      ),
+      body = new_data,
+      encode = "json"
+    )
     text <- paste0("Active outbreak changed! ", outbreak_id, " is now active.")
   }
 
