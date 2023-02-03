52e1d831036f3457c0acb11becf1fdb53194c7f6
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-02-03 10:16:20 +0000

    linted get_language_tokens

--- R/get_language_tokens.R
+++ R/get_language_tokens.R
@@ -1,13 +1,17 @@
-#' Get a list of variable tokens and their labels for the language you specify, in order to re-code variables in R.
+#' Get a list of variable tokens and their labels for the language you specify,
+#' in order to re-code variables in R.
 #'
 #'
-#' @param url Insert the base URL for your instance of Go.Data here. Don't forget the forward slash "/" at end!
+#' @param url Insert the base URL for your instance of Go.Data here. Don't
+#' forget the forward slash "/" at end!
 #' @param username The email address for your Go.Data login.
 #' @param password The password for your Go.Data login
-#' @param language The language ID you are retrieving translation file for, for instance "english_us"
+#' @param language The language ID you are retrieving translation file for,
+#' for instance "english_us"
 #'
 #' @return
-#' Returns data frame of language tokens for your language. You will only be able to execute this function if you have access to the language tokens.
+#' Returns data frame of language tokens for your language. You will only be
+#' able to execute this function if you have access to the language tokens.
 #' @export
 #' @examples
 #' \dontrun{
@@ -16,10 +20,12 @@
 #' password <- "mypassword"
 #' language <- "english_us"
 #'
-#' language_tokens <- get_language_tokens(url=url,
-#'                                        username=username,
-#'                                        password=password,
-#'                                        language=language)
+#' language_tokens <- get_language_tokens(
+#'   url = url,
+#'   username = username,
+#'   password = password,
+#'   language = language
+#' )
 #' }
 #' @importFrom magrittr %>%
 #' @import dplyr
@@ -29,15 +35,27 @@
 #' @importFrom purrr pluck
 
 
-get_language_tokens <- function(url=url,
-                                username=username,
-                                password=password,
-                                language=language) {
+get_language_tokens <- function(url = url,
+                                username = username,
+                                password = password,
+                                language = language) {
 
-  df <- GET(paste0(url,"api/languages/",language,"/language-tokens",
-                          "?access_token=",get_access_token(url=url, username=username, password=password))) %>%
-    content(as="text") %>%
-    fromJSON(flatten=TRUE) %>%
+  df <- GET(
+    paste0(
+      url,
+      "api/languages/",
+      language,
+      "/language-tokens",
+      "?access_token=",
+      get_access_token(
+        url = url,
+        username = username,
+        password = password
+      )
+    )
+  ) %>%
+    content(as = "text") %>%
+    fromJSON(flatten = TRUE) %>%
     as_tibble()
 
   return(df)
