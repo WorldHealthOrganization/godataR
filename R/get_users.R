20141048e47919a55ffb3b68cf01c3412448379d
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-02-14 15:03:18 +0000

    linted get_users

--- R/get_users.R
+++ R/get_users.R
@@ -6,12 +6,15 @@
 #' endpoint). This function relies on the
 #' `\users` API endpoint.
 #'
-#' @param url Insert the base URL for your instance of Go.Data here. Don't forget the forward slash "/" at end!
+#' @param url Insert the base URL for your instance of Go.Data here. Don't
+#' forget the forward slash "/" at end!
 #' @param username The email address for your Go.Data login.
 #' @param password The password for your Go.Data login
 #'
 #' @return
-#' Returns a data frame. Some fields, such as addresses, hospitalization history, and questionnaire fields may require further unnesting. See \code{\link[tidyr]{nest}} for assitance with unnesting.
+#' Returns a data frame. Some fields, such as addresses, hospitalization
+#' history, and questionnaire fields may require further unnesting. See
+#' \code{\link[tidyr]{nest}} for assitance with unnesting.
 #' @export
 #' @examples
 #' \dontrun{
@@ -19,9 +22,11 @@
 #' username <- "myemail@email.com"
 #' password <- "mypassword"
 #'
-#' users <- get_users(url=url,
-#'                    username=username,
-#'                    password=password)
+#' users <- get_users(
+#'   url = url,
+#'   username = username,
+#'   password = password
+#' )
 #' }
 #' @importFrom magrittr %>%
 #' @import dplyr
@@ -30,19 +35,25 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom purrr pluck
 #' @export
+get_users <- function(url = url,
+                      username = username,
+                      password = password) {
 
+  users <- GET(
+    paste0(
+      url,
+      "api/users",
+      "?access_token=",
+      godataR::get_access_token(
+        url = url,
+        username = username,
+        password = password
+      )
+    )
+  ) %>%
+    content(as = "text") %>%
+    fromJSON(flatten = TRUE) %>%
+    filter(.data$deleted != TRUE)
 
-
-get_users <- function(url=url,
-                      username=username,
-                      password=password) {
-
-  users <- GET(paste0(url,"api/users",
-                          "?access_token=",godataR::get_access_token(url=url, username=username, password=password))) %>%
-    content(as="text") %>%
-    fromJSON(flatten=TRUE) %>%
-    filter(deleted!=TRUE) %>%
-    #select(id, firstName, lastName, email, roleIds, lastLoginDate, institutionName, disregardGeographicRestrictions, activeOutbreakId, createdBy, createdAt)
   return(users)
-
 }
